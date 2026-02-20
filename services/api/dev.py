841c95f30de6a20f5ec48b279c646849c4008cfc
ALincolnN <njoguabraham2@gmail.com>
2026-02-20 23:07:51 +0300

    fix: fix port mapping on dev compose file

--- services/api/dev.py
+++ services/api/dev.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app="services.api.main:app",
         host="0.0.0.0",
-        port=8900,
+        port=8000,
         reload=True,
         reload_includes=["*.py"],
         log_config=LOGGING,
